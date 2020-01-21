52cce271a0562c9eafa99e024906beae9ff4c16d
chris_white <whitec23@uw.edu>
2020-01-20 20:01:08 -0800

    add week 1 exercises

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -1,25 +1,28 @@
 # Exercise 1: calling built-in functions
 
 # Create a variable `my_name` that contains your name
+my_name <- 'Christopher'
 
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name (use the `nchar()` function)
-
+name_length <- nchar(my_name)
 
 # Print the number of letters in your name
+print(name_length)
 
 
-# Create a variable `now_doing` that is your name followed by "is programming!" 
+# Create a variable `now_doing` that is your name followed by "is programming!"
 # (use the `paste()` function)
+now_doing <- paste(my_name, 'is programming')
 
 
 # Make the `now_doing` variable upper case
-
+toupper(now_doing)
 
 ### Bonus
 
-# Pick two of your favorite numbers (between 1 and 100) and assign them to 
+# Pick two of your favorite numbers (between 1 and 100) and assign them to
 # variables `fav_1` and `fav_2`
 
 
@@ -27,7 +30,7 @@
 # original variable
 
 
-# Create a variable `raw_sum` that is the sum of the two variables. Use the 
+# Create a variable `raw_sum` that is the sum of the two variables. Use the
 # `sum()` function for practice.
 
 
@@ -35,7 +38,7 @@
 # Use the `round()` function.
 
 
-# Create two new variables `round_1` and `round_2` that are your `fav_1` and 
+# Create two new variables `round_1` and `round_2` that are your `fav_1` and
 # `fav_2` variables rounded to 1 decimal places
 
 
@@ -43,4 +46,3 @@
 
 
 # Which is bigger, `round_sum` or `sum_round`? (You can use the `max()` function!)
-
--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -1,18 +1,22 @@
 # Exercise 3: writing and executing functions
 
 # Define a function `add_three` that takes a single argument and
-# returns a value 3 greater than the input
+# returns a value 3 greater than the input\
+add_three <- function(x) { returns (x + 3)}
 
 
-# Create a variable `ten` that is the result of passing 7 to your `add_three` 
+# Create a variable `ten` that is the result of passing 7 to your `add_three`
 # function
+ten <- add_three(7)
 
 
-# Define a function `imperial_to_metric` that takes in two arguments: a number 
+# Define a function `imperial_to_metric` that takes in two arguments: a number
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(num_feet, num_inches) {
+return (num_feet * .3048 + num_inches * .0254)
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
-
+height_in_meters <- imperial_to_metric(2,3)
